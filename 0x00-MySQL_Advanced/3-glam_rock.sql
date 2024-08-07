c2a1c69cdb36adea10dd7fbda4c96db5d52a4c18
dexterski007 <mbouj007@gmail.com>
2024-08-08 00:08:12 +0100

    fix

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,9 +1,6 @@
 -- glam rock
 SELECT band_name,
-CASE
-    WHEN split IS NULL THEN 2022 - formed
-    ELSE split - formed
-END as lifespan
+(IFNULL(split, 2022) - formed) as lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
