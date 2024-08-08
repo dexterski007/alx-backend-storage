728bd8dc0b016e22094b0e18b1e28820edd87c5c
dexterski007 <mbouj007@gmail.com>
2024-08-08 01:22:17 +0100

    fix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -4,10 +4,9 @@ CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
-    DECLARE avg_score FLOAT;
     SELECT AVG(score) INTO avg_score
     FROM corrections
-    WHERE user_id = user_id;
+    WHERE corrections.user_id = user_id;
     UPDATE users
     SET average_score = IFNULL(avg_score, 0)
     WHERE id = user_id;
