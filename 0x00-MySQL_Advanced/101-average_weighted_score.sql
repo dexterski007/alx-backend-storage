43b525292fa017e1c589acdbb910e70649b9946c
dexterski007 <mbouj007@gmail.com>
2024-08-08 02:40:20 +0100

    testing

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -1,12 +1,13 @@
 -- bonus procedure
-DELIMITER $$
+DELIMITER //
 CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     UPDATE users
-    SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
-    FROM corrections
-    JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id
+    SET average_score = (
+        SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+        FROM corrections
+        JOIN projects ON corrections.project_id = projects.id
+        WHERE corrections.user_id = users.id
     );
-END $$
+END //
 DELIMITER ;
