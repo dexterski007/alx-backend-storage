fed1b979f49db0eff6ba3ade8af01f68e1e335f0
dexterski007 <mbouj007@gmail.com>
2024-08-08 02:08:28 +0100

    wow

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,7 +1,11 @@
 -- safediv function
-DELIMITER //
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
+DELIMITER $$
+CREATE FUNCTION SafeDiv(
+    a INT,
+    b INT
+)
+RETURNS FLOAT
 BEGIN
     RETURN IF(b = 0, 0, a / b);
-END //
+END $$
 DELIMITER ;
