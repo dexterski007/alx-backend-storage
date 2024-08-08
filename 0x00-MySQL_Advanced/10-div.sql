bd4a53a8d673945ac17626ead9eae5c06abc50b9
dexterski007 <mbouj007@gmail.com>
2024-08-08 02:06:40 +0100

    ?

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,9 +1,6 @@
 -- safediv function
 DELIMITER $$
-CREATE FUNCTION SafeDiv(
-    IN a INT,
-    IN b INT
-)
+CREATE FUNCTION SafeDiv(IN a INT, IN b INT)
 RETURNS FLOAT
 BEGIN
     RETURN IF(b = 0, 0, a / b);
