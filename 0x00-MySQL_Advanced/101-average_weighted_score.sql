0b3286ff68f9d167e93b0aaf7ec7445463dcc4ff
dexterski007 <mbouj007@gmail.com>
2024-08-08 02:43:35 +0100

    fixed

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -6,7 +6,7 @@ BEGIN
     SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
         FROM corrections
         JOIN projects ON corrections.project_id = projects.id
-        WHERE corrections.user_id = user.id
+        WHERE corrections.user_id = users.id
     );
 END $$
 DELIMITER ;
