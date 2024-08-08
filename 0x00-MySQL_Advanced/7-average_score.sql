8c297878c7e3d76888e881c8b9ec9ff7848e03b9
dexterski007 <mbouj007@gmail.com>
2024-08-08 01:30:43 +0100

    fix

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,15 +1,13 @@
 -- bonus procedure
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
 CREATE PROCEDURE ComputeAverageScoreForUser(
     IN user_id INT
 )
 BEGIN
-    SELECT AVG(score) INTO avg_score
-    FROM corrections
-    WHERE corrections.user_id = user_id;
     UPDATE users
-    SET average_score = avg_score
+    SET average_score = (SELECT AVG(score)
+    FROM corrections
+    WHERE corrections.user_id = user_id)
     WHERE id = user_id;
 END;$$
 DELIMITER ;
