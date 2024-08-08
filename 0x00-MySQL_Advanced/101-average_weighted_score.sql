a408115a297dd35920c4c8cec2ca0bf6b228f5ae
dexterski007 <mbouj007@gmail.com>
2024-08-08 02:33:48 +0100

    linted

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,7 +3,7 @@ DELIMITER $$
 CREATE PROCEDURE ComputeAverageWeightedScoreForUser()
 BEGIN
     UPDATE users
-    SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight))
+    SET average_score = (SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
     FROM corrections
     JOIN projects ON corrections.project_id = projects.id
     WHERE corrections.user_id = user_id
