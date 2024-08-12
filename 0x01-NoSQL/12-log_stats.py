be37437e867f2a30540a54def39d97ca51e3050c
dexterski007 <mbouj007@gmail.com>
2024-08-12 22:53:24 +0100

    fix

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
     collection = client.logs.nginx
-    counter = collection.count_documents()
+    counter = collection.count_documents({})
     print("{} logs".format(counter))
     print("Methods:")
     print("")
