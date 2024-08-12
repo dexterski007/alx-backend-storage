129e0096f07763ec9303d45c3f6acd32e536ea9a
Your Name <you@example.com>
2024-08-12 15:14:21 -0700

    new

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" filter topics in python """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """ filter all topics """
+    return list(mongo_collection.find({"topics": topic}))
