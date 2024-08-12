4a4fce595988b01d2f8ce78042723412ce9e6998
dexterski007 <mbouj007@gmail.com>
2024-08-12 22:04:37 +0100

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" list all documents in python """
+
+
+def list_all(mongo_collection):
+    """ list all docs in mongodb """
+    if mongo_collection is None:
+        return []
+    new_list = []
+    for doc in mongo_collection:
+        new_list.append(doc)
+    return new_list
