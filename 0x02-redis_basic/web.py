d8335bcacc956ce33d3d3f51e7e38c4b2e97566f
dexterski007 <mbouj007@gmail.com>
2024-08-14 00:37:44 +0100

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,56 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+""" Site Cache """
 import redis
 import requests
 from functools import wraps
 from typing import Callable
 
-
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+rds = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
-        return result
-    return invoker
+def cache_url(exp_time: int) -> Callable[[Callable], Callable]:
+    """
+    Decorator function for caching the result of a URL request in Redis.
+    """
+    def decorator(method: Callable) -> Callable:
+        """ decorator function """
+
+        @wraps(method)
+        def wrapper(url: str) -> str:
+            """
+            Wrapper function to check and retrieve the result from the
+            cache or make a new request.
+            """
+            count = "count:{}".format(url)
+            key = "result:{}".format('url')
+
+            rds.incr(count)
+
+            result = rds.get(key)
+
+            if result:
+                return result.decode('utf-8')
+
+            res = method(url)
+            rds.set(count, 0)
+            rds.setex(key, exp_time, res)
+
+            return res
+
+        return wrapper
+
+    return decorator
 
 
-@data_cacher
+@cache_url(10)
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    Retrieve the content of a web page and cache the result.
+    """
+    resp = requests.get(url)
+
+    if resp.status_code == 200:
+        return resp.text
+    else:
+        return "Unable to access {}. Status code: {}".format(
+                url, resp.status_code)
