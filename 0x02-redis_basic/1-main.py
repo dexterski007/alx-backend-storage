53faf2b246d3817ecc26590a54988026ac3b4aed
dexterski007 <mbouj007@gmail.com>
2024-08-13 21:48:23 +0100

    task 1

--- 0x02-redis_basic/1-main.py
+++ 0x02-redis_basic/1-main.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-""" 8-main """
-from pymongo import MongoClient
-list_all = __import__('8-all').list_all
+"""
+Main file
+"""
+import redis
 
-if __name__ == "__main__":
-    client = MongoClient('mongodb://127.0.0.1:27017')
-    school_collection = client.my_db.school
-    schools = list_all(school_collection)
-    for school in schools:
-        print("[{}] {}".format(school.get('_id'), school.get('name')))
+Cache = __import__('exercise').Cache
+
+cache = Cache()
+
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
+
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+
+print("everything is good!")
--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """ redis cache setup """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -17,3 +17,22 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+    
+    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+        """ decoder of byte strings """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn is not None:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Optional[str]:
+        """ str decoder """
+        data = self.get(key, fn=lambda a: a.decode('utf-8'))
+        return data
+
+    def get_int(self, key: str) -> Optional[int]:
+        """ int decoder """
+        data = self.get(key, fn=lambda b: int(b.decode('utf-8')))
+        return data
