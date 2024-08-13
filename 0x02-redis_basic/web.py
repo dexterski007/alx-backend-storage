249819850211ac141e029f3df7d66c5268c0370b
dexterski007 <mbouj007@gmail.com>
2024-08-14 00:25:30 +0100

    test

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,36 +1,34 @@
 #!/usr/bin/env python3
-"""
-Implementing an expiring web cache and tracker
-"""
+""" Python script """
 import requests
 import redis
+from typing import Callable
 from functools import wraps
 
-mem = redis.Redis()
+
+red = redis.Redis()
 
 
-def count_url_access(method):
-    """Counting how many times a URL is accessed using decorator"""
+def count_requests(method: Callable) -> Callable:
+    """Decorator"""
     @wraps(method)
     def wrapper(url):
-        count_key = "count:" + url
-        cached_key = "cached:" + url
-        cached_data = store.get(cached_key)
+        """ wrapper method"""
+        red.incr(f"count:{url}")
+        cached_html = red.get(f"cached:{url}")
 
-        if cached_data:
-            return cached_data.decode("utf-8")
-
+        if cached_html:
+            return cached_html.decode('utf-8')
         html = method(url)
-
-        mem.incr(count_key)
-        mem.set(cached_key, html)
-        mem.expire(cached_key, 10)
+        red.setex(f"cached:{url}", 10, html)
         return html
+
     return wrapper
 
 
-@count_url_access
+@count_requests
 def get_page(url: str) -> str:
-    """Returns HTML content of a URL"""
-    res = requests.get(url)
-    return res.text
+    """ uses the requests module to obtain the HTML content
+    of a particular URL and returns it """
+    req = requests.get(url)
+    return req.text
