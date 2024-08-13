697b77e06bac9b032a30092d0237204674c6e0f2
dexterski007 <mbouj007@gmail.com>
2024-08-13 21:54:50 +0100

    task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,22 +17,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
-    def get(self, key: str, fn: Optional[Callable]) -> Union[str, bytes, int, float]:
+
+    def get(self, key: str, fn:
+            Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """ decoder of byte strings """
         data = self._redis.get(key)
-        if data is None:
-            return None
         if fn is not None:
-            return fn(data)
+            data = fn(data)
         return data
 
-    def get_str(self, key: str) -> Optional[str]:
+    def get_str(self, key: str) -> str:
         """ str decoder """
         data = self.get(key, fn=lambda a: a.decode('utf-8'))
-        return data
+        return str(data)
 
-    def get_int(self, key: str) -> Optional[int]:
+    def get_int(self, key: str) -> int:
         """ int decoder """
-        data = self.get(key, fn=lambda b: int(b.decode('utf-8')))
-        return data
+        data = self.get(key, fn=lambda b: b.decode('utf-8'))
+        return int(data)
