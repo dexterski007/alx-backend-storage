570ca689d1189c654b1f4ebc2d8f807243fa9725
dexterski007 <mbouj007@gmail.com>
2024-09-01 19:13:25 +0100

    fix

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,46 +1,17 @@
 #!/usr/bin/env python3
-""" function to get a page """
+""" redis cache implement """
 import requests
-import time
-from functools import wraps
-from typing import Callable
 import redis
 
-
-redis_client = redis.Redis()
-
-
-def cacher(expiry: int = 10) -> Callable:
-    """ decorator to cache and track number of accesses"""
-    def decorator(fn: Callable) -> Callable:
-        @wraps(fn)
-        def wrapper(url: str) -> str:
-            cache_key = "cache:" + url
-            cached_content = redis_client.get(cache_key)
-            if cached_content:
-                return cached_content.decode("utf-8")
-            content = fn(url)
-            redis_client.setex(cache_key, expiry, content)
-            return content
-        return wrapper
-    return decorator
+rcache = redis.Redis()
 
 
-def counter() -> Callable:
-    """ decorator to count functio naccess"""
-    def decorator(fn: Callable) -> Callable:
-        @wraps(fn)
-        def wrapper(url: str) -> str:
-            count_key = str("count:" + url)
-            redis_client.incr(count_key)
-            return fn(url)
-        return wrapper
-    return decorator
-
-
-@counter()
-@cacher(expiry=10)
 def get_page(url: str) -> str:
-    """ function to get an html page"""
-    response = requests.get(url)
-    return response.text
+    """ getting html and content """
+    rcache.incr(f"count:{url}")
+    cache = rcache.get(url)
+    if cache:
+        return cache.decode('utf-8')
+    content = requests.get(url).text
+    rcache.setex(url, 10, content)
+    return content
