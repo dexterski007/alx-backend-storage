325daebee8c40b0d0991f1d5c7f18f2eb48da9f9
dexterski007 <mbouj007@gmail.com>
2024-09-01 19:19:02 +0100

    reverting

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,21 +1,46 @@
 #!/usr/bin/env python3
-"""5. Implementing an expiring web cache and tracker"""
+""" function to get a page """
 import requests
+import time
+from functools import wraps
+from typing import Callable
 import redis
 
-r = redis.Redis()
+
+redis_client = redis.Redis()
 
 
+def cacher(expiry: int = 10) -> Callable:
+    """ decorator to cache and track number of accesses"""
+    def decorator(fn: Callable) -> Callable:
+        @wraps(fn)
+        def wrapper(url: str) -> str:
+            cache_key = "cache:" + url
+            cached_content = redis_client.get(cache_key)
+            if cached_content:
+                return cached_content.decode("utf-8")
+            content = fn(url)
+            redis_client.setex(cache_key, expiry, content)
+            return content
+        return wrapper
+    return decorator
+
+
+def counter() -> Callable:
+    """ decorator to count functio naccess"""
+    def decorator(fn: Callable) -> Callable:
+        @wraps(fn)
+        def wrapper(url: str) -> str:
+            count_key = str("count:" + url)
+            redis_client.incr(count_key)
+            return fn(url)
+        return wrapper
+    return decorator
+
+
+@counter()
+@cacher(expiry=10)
 def get_page(url: str) -> str:
-    """Get the HTML content of a particular URL and returns it"""
-    r.incr(f"count:{url}")
-
-    cached = r.get(url)
-    if cached:
-        return cached.decode('utf-8')
-
-    html_content = requests.get(url).text
-
-    r.setex(url, 10, html_content)
-
-    return html_content
+    """ function to get an html page"""
+    response = requests.get(url)
+    return response.text
